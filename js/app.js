b5464151ee69bfb66742f9abca0bd522dbaab161
Mason Embry <masonembry81@mac.com>
2016-10-06 11:43:54 -0500

    Added IIFE and 'use strict'

---
+++ js/app.js
@@ -0,0 +1,17 @@
+(function($) {
+    
+    'use strict';
+
+    ////////// Tic-Tac-Toe //////////
+    //
+    // Author: Mason Embry
+    // Created 10/6/2016
+    // Last Updated:
+    //
+    // Tested in current versions of Chrome, Safari, and Firefox.
+    //
+    /////////////////////////////////
+
+
+
+})(jQuery);
