706eb445770ce833f80cc560385baa7599646590
Mason Embry <masonembry81@mac.com>
2016-10-06 13:50:45 -0500

    Start screen and game over screen in progress.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function($) {
+// (function($) {
 
     'use strict';
 
@@ -12,66 +12,95 @@
     //
     /////////////////////////////////
 
-var GameConstructor = function() {
 
-};
 
-// Hide entire #board div
-// Create #start div and show it
-    // <div class="screen screen-start" id="start">
-    //   <header>
-    //     <h1>Tic Tac Toe</h1>
-    //     <a href="#" class="button">Start game</a>
-    //   </header>
-    // </div>
-// When button clicked
+    var PlayerConstructor = function(name) {
+        this.name = name;
+        this.winner = false;
+        this.turn = false;
+        this.computerPlayer = false;
+    };
+
+
+
+    var startScreen = function() {
+        // Hide entire #board div
+        $("#board").hide();
+
+        // Create #start div and show it
+        var startDivContent = '<div class="screen screen-start" id="start">';
+        startDivContent += '<header>';
+        startDivContent += '<h1>Tic Tac Toe</h1>';
+        startDivContent += '<a href="#" class="button">Start game</a>';
+        startDivContent += '</header>';
+        startDivContent += '</div>';
+        $("body").html(startDivContent);
+    };
+
+    // When button clicked
     // Extra credit:
-        // Let player choose to play against computer or against other player.
-            // If against other player:
-                // Get each player's name
-                    // Display each player's name while playing
-                    // Display the winning player's name at end
-            // If against computer:
-                // Get player's name for player 1
-                // Set player 2's name to "Computer"
-                // Start AI version of game...
+    // Let player choose to play against computer or against other player.
+    // If against other player:
+    // Get each player's name
+    // Display each player's name while playing
+    // Display the winning player's name at end
+    // If against computer:
+    // Get player's name for player 1
+    // Set player 2's name to "Computer"
+    // Start AI version of game...
     // #start div is hidden
     // #board div is shown
     // game begins
 
-// PLAY GAME - two players
+    // PLAY GAME - two players
 
-// Add the game play following these rules.
-// Play alternates between X and O.
-// The current player is indicated at the top of the page -- the box with the symbol O or X is highlighted for the current player. You can do this by simply adding the class .active to the proper list item in the HTML. For example, if it's player one's turn, the HTML should look like this: <li class="players active" id="player1">
-// When the current player mouses over an empty square on the board, it's symbol the X or O should appear on the square. You can do this using the x.svg or o.svg graphics (hint use JavaScript to set the background-image property for that box.)
-// Players can only click on empty squares. When the player clicks on an empty square, attach the class box-filled-1 (for O) or box-filled-2 (for X) to the square. The CSS we're providing will automatically add the proper image to the square marking it as occupied.
-// The game ends when one player has three of their symbols in a row either horizontally, vertically or diagonally. If all of the squares are filled and no players have three in a row the game is a tie.
+    // Add the game play following these rules.
+    // Play alternates between X and O.
+    // The current player is indicated at the top of the page -- the box with the symbol O or X is highlighted for the current player. You can do this by simply adding the class .active to the proper list item in the HTML. For example, if it's player one's turn, the HTML should look like this: <li class="players active" id="player1">
+    // When the current player mouses over an empty square on the board, it's symbol the X or O should appear on the square. You can do this using the x.svg or o.svg graphics (hint use JavaScript to set the background-image property for that box.)
+    // Players can only click on empty squares. When the player clicks on an empty square, attach the class box-filled-1 (for O) or box-filled-2 (for X) to the square. The CSS we're providing will automatically add the proper image to the square marking it as occupied.
+    // The game ends when one player has three of their symbols in a row either horizontally, vertically or diagonally. If all of the squares are filled and no players have three in a row the game is a tie.
 
-// PLAY GAME - one player
+    // PLAY GAME - one player
 
 
-// When game ends
-    // Hide #board div
-    // Create and show #finish div
-        // <div class="screen screen-win" id="finish">
-        //   <header>
-        //     <h1>Tic Tac Toe</h1>
-        //     <p class="message"></p>
-        //     <a href="#" class="button">New game</a>
-        //   </header>
-        // </div>
+    // When game ends
+    var gameOver = function(setWinnerClass, winnerName) {
+        // Hide #board div
+        $("#board").hide();
+
+        // Create and show #finish div
+        var finishDivContent = '<div class="screen screen-win" id="finish">';
+        finishDivContent += '<header>';
+        finishDivContent += '<h1>Tic Tac Toe</h1>';
+        finishDivContent += '<p class="message"></p>';
+        finishDivContent += '<a href="#" class="button">New game</a>';
+        finishDivContent += '</header>';
+        finishDivContent += '</div>';
+        $("body").html(finishDivContent);
+
+        // Indicate winner
+        $("#finish").addClass(setWinnerClass);
+        if (setWinnerClass === "screen-win-tie") {
+            $(".message").html("It's a Tie!");
+        } else {
+            $(".message").html(winnerName);
+        }
+
+    };
+
+
     // If player1 is winner
-        // Show the word "Winner" and <div class="screen screen-win screen-win-one" id="finish">
+    // Show the word "Winner" and <div class="screen screen-win screen-win-one" id="finish">
     // If player2 is winner
-        // Show the word "Winner" and <div class="screen screen-win screen-win-two" id="finish">
+    // Show the word "Winner" and <div class="screen screen-win screen-win-two" id="finish">
     // If it is a tie
-        // Show the word "It's a Tie!" and <div class="screen screen-win screen-win-tie" id="finish">
+    // Show the word "It's a Tie!" and <div class="screen screen-win screen-win-tie" id="finish">
 
-// When player pushes the "New Game" button:
+    // When player pushes the "New Game" button:
     // Extra credit:
-        // Ask if playing computer and continue on as appropriate
-        // As for names of both players
+    // Ask if playing computer and continue on as appropriate
+    // As for names of both players
     // Start new game (initialize new "game" object?)
     // Hide (and reset?) #finish div
     // Show #board div
@@ -82,8 +111,6 @@ var GameConstructor = function() {
 
 
 
+startScreen();
 
-
-
-
-})(jQuery);
+// })(jQuery);
